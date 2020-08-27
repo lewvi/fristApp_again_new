b381853a7ee5efa19820c816d5b30d604bbf2932
lewvi <lew_rip@hotmail.com>
2020-08-27 18:38:37 +0700

    Create Model

--- app/src/main/java/com/natthakun/fristapp_again/ContactFragment.kt
+++ app/src/main/java/com/natthakun/fristapp_again/ContactFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.natthakun.fristapp_again.ContactDB.ContactDatabase
 import com.natthakun.fristapp_again.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
@@ -22,6 +24,17 @@ class ContactFragment : Fragment() {
             false
         )
         setHasOptionsMenu(true)
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = ContactDatabase.getInstance(application).Dao
+        val viewModelFactory = ContactViewModelFactory(dataSource, binding, application)
+        val contactViewModel =
+            ViewModelProvider(
+                this, viewModelFactory
+            ).get(ContactViewModel::class.java)
+        binding.contactViewModel = contactViewModel
+        binding.lifecycleOwner = this
+
         return binding.root
     }
 
---
+++ app/src/main/java/com/natthakun/fristapp_again/ContactViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.natthakun.fristapp_again
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.natthakun.fristapp_again.ContactDB.ContactDAO
+import com.natthakun.fristapp_again.databinding.FragmentContactBinding
+
+class ContactViewModelFactory (
+    private val dataSource: ContactDAO,
+    private val binding: FragmentContactBinding,
+    private val application: Application
+) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ContactViewModel::class.java)) {
+            return ContactViewModel(dataSource, binding, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
